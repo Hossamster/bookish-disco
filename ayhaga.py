cfb302170317af171e034a2125efb8bc7d6d4a18
Hossam <hossam180871@bue.edu.eg>
2024-01-02 16:23:41 +0200

    edit in main

--- ayhaga.py
+++ ayhaga.py
@@ -1,12 +1,11 @@
-def bomm():
-    print('lokdmsa')
-def ldsadas():
-    print('kdasokdk')
-import numpy as np
-print(np.arange(1,10))
-k = 0
-j =2 
-print(k)
-print(j)
-print(np.arange(1,10))
-print(np.arange(1,10))
+# main.py in Main Branch
+
+def greet():
+    print("Hello from the Main Branch")
+
+def calculate(a, b):
+    return a + b
+
+print(greet())
+result = calculate(3, 4)
+print(f"Calculation result: {result}")
