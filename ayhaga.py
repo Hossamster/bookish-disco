1ab70a6f4f90ba58ae165dd166433d0834fce334
Hoss <Hossamsters@gmail.com>
2024-01-02 16:10:06 +0200

    edit it again

--- ayhaga.py
+++ ayhaga.py
@@ -1,12 +1,13 @@
+def bomm():
+    print('Updated in boom branch')  # Change the print statement
+def ldsadas():
+    print('kdasokdk')
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns 
-import pandas as pd
+print(np.arange(1, 20))  # Change the range of np.arange
 
-df = pd.read_csv(r'')
-df.head()
-def boom():
-    print("232")
-
-def letsgo():
-    return boom()
+k = 10  # Change the value of k
+j = 5   # Change the value of j
+print(k)
+print(j)
+print(np.arange(5, 15))  # Change the range of np.arange
+print(np.arange(1, 10))
