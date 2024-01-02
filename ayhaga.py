bffcc397a7658b8e96383f5a7de99cfac20a17fd
Hoss <Hossamsters@gmail.com>
2024-01-02 16:00:53 +0200

    create ayhaga file for lol

---
+++ ayhaga.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns 
+import pandas as pd
+
+df = pd.read_csv(r'')
+df.head()
+def boom():
+    print("232")
+
+def letsgo():
+    return boom()
