820f3248d573ce8419537cafb0b0cc3adc9098a6
Hoss <Hossamsters@gmail.com>
2024-01-02 18:24:10 +0200

    edit the ayhaga

--- ayhaga.py
+++ ayhaga.py
@@ -1,11 +1,12 @@
-# main.py in Feature Branch
-
-def greet():
-    print("Greetings from the Feature Branch")
-
-def calculate(a, b):
-    return a * b  # Changed operation from + to *
-
-print(greet())
-result = calculate(2, 5)
-print(f"Result of operation: {result}")
+def bomm():
+    print('lokdmsa')
+def ldsadas():
+    print('kdasokdk')
+import numpy as np
+print(np.arange(1,10))
+k = 0
+j = 2
+print(k)
+print(j)
+print(np.arange(1,10))
+print(np.arange(1,10))
