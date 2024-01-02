a68c161e3b5f4d3d0046b50c84039aea4d95c316
Hossam <hossam180871@bue.edu.eg>
2024-01-02 15:30:34 +0200

    create python file

---
+++ ayhaga.py
@@ -0,0 +1,12 @@
+def bomm():
+    print('lokdmsa')
+def ldsadas():
+    print('kdasokdk')
+import numpy as np
+print(np.arange(1,10))
+k = 0
+j =2 
+print(k)
+print(j)
+print(np.arange(1,10))
+print(np.arange(1,10))
